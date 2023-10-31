1527f0645c565e417d5b43ed474b30b619ba73c0
nmillier <nmillier@student.42mulhouse.fr>
2023-10-31 18:04:52 +0100

    finished ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,11 +6,27 @@
 /*   By: nmillier <nmillier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:52:53 by nmillier          #+#    #+#             */
-/*   Updated: 2023/10/24 15:53:20 by nmillier         ###   ########.fr       */
+/*   Updated: 2023/10/31 18:01:06 by nmillier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	char			*newstring;
+	long unsigned	i;
+
+	i = 0;
+	newstring = (char *) malloc((len + 1) *sizeof(char));
+	if (!newstring)
+		return (NULL);
+	while (i < len)
+	{
+		newstring[i] = s[start + i];
+		i++;
+	}
+	newstring[i] = '\0';
+	return (newstring);
 }
