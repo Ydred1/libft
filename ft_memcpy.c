87086019bb2b3887875ff6366d8e1850a98a604e
nmillier <nmillier@student.42mulhouse.fr>
2023-11-03 15:30:33 +0100

    fixed norm on memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,16 +6,16 @@
 /*   By: nmillier <nmillier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 15:37:52 by nmillier          #+#    #+#             */
-/*   Updated: 2023/10/25 11:25:46 by nmillier         ###   ########.fr       */
+/*   Updated: 2023/11/03 14:58:25 by nmillier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	const char *s;
-	char *d;
+	const char	*s;
+	char		*d;
 
 	s = src;
 	d = dest;
