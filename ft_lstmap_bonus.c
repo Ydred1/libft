a973954942efce3a0148511f98cd1870115c4e8b
nmillier <nmillier@student.42mulhouse.fr>
2023-11-08 10:37:28 +0100

    fixed memory leak for edge case

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,19 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ydred <ydred@student.42.fr>                +#+  +:+       +#+        */
+/*   By: nmillier <nmillier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 04:18:30 by ydred             #+#    #+#             */
-/*   Updated: 2023/11/05 10:34:01 by ydred            ###   ########.fr       */
+/*   Updated: 2023/11/08 10:36:23 by nmillier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	*ft_failedmalloc(t_list *lst, void (*del)(void *))
+static t_list	*ft_failedmalloc(t_list *lst, void (*del)(void *), void *c)
 {
+	del(c);
 	ft_lstclear(&lst, del);
 	return (NULL);
 }
@@ -23,19 +24,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*newlst;
 	t_list	*current;
 	t_list	*currentnew;
+	void	*content;
 
 	if (lst == NULL)
 		return (NULL);
 	current = lst;
-	newlst = ft_lstnew(f(current->content));
-	if (newlst == NULL)
-		return (ft_failedmalloc(newlst, del));
+	content = f(current->content);
+	newlst = ft_lstnew(content);
+	if (newlst == NULL || newlst->content == NULL)
+		return (ft_failedmalloc(newlst, del, content));
 	currentnew = newlst;
 	while (current->next)
 	{
-		currentnew->next = ft_lstnew(f(current->next->content));
-		if (currentnew->next == NULL)
-			return (ft_failedmalloc(newlst, del));
+		content = f(current->next->content);
+		currentnew->next = ft_lstnew(content);
+		if (currentnew->next == NULL || currentnew->next->content == NULL)
+			return (ft_failedmalloc(newlst, del, content));
 		currentnew = currentnew->next;
 		current = current->next;
 	}
